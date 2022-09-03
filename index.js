3dc1d4745aecee9f21c31daf402a4731044fcde4
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-09-03 17:26:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const app = express();
-var myJson = {
-    key: "myvalue"
-};
-var myJson2 = {
-    key: "myvalue"
-};
-var myJson3 = {
-    key: "myvalue"
-};
-var myJson4 = {
-    key: "myvalue"
-};
+const myArray = [];
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.get('/charlist', function(request, response) {
-	response.send(myJson);
+	response.send(JSON.stringify(myArray));
 });
 app.post('/setchar', function(request, response) {
-	myJson = request.body;
-	response.send(myJson);
+	const json_decode = JSON.parse(request.body)
+	const theyExist = false
+	myArray.forEach((v, i, array) => {
+		if (v.userid == json_decode.userid) {
+			theyExist = true
+			myArray.splice(i, 1);
+			myArray.push(request.body);
+		}
+	});
+	if (theyExist == false) {
+		myArray.push(request.body);
+	}
+	response.send(JSON.stringify(myArray));
 });
 app.listen(process.env.PORT,function(){
 	console.log("App listening on port process.env.PORT");
